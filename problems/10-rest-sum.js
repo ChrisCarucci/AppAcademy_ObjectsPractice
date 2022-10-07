e8be753341fa941738b015b13732e10ba6f2b2db
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-10-07 01:09:25 -0400

    Objects Problm Set Solved

---
+++ problems/10-rest-sum.js
@@ -0,0 +1,27 @@
+/***********************************************************************
+Write a function named `restSum` that accepts all incoming parameters and sums them.
+
+**Hint**: Use rest parameter syntax!
+
+Examples:
+restSum(3,5,6); // => 14
+restSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+restSum(0); // => 0
+***********************************************************************/
+
+function restSum(...otherNums) {
+  // Your code here
+  let sum = 0;
+  let arraySum = otherNums
+
+  console.log(arraySum[0])
+  for (let i = 0; i < arraySum.length; i++) {
+    sum += arraySum[i]
+  }
+
+  return sum;
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = restSum;
